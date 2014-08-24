29edb7fb20bd51b9bf3d8552f96898a0ca7ae629
Marek Dano <mk.dano@gmail.com>
2014-08-24 15:50:42 +0100

    Added function for editing a post.

--- models/blogpost.js
+++ models/blogpost.js
@@ -8,6 +8,28 @@ var schema = mongoose.Schema({
 	author: { type: String, ref: 'User' }
 })
 
+schema.statics.edit = function (req, callback) {
+	var id = req.param('id');
+	var author = req.session.user;
+
+	// validate current user authored this blogpost
+	var query = { _id: id, author: author };
+
+	var update = {};
+	update.title = req.param('title');
+	update.body = req.param('body');
+
+	this.update(query, update, function (err, numAffected) {
+		if (err) return callback(err);
+
+		if (0 === numAffected) {
+			return callback(new Error('no post to modify'));
+		}
+
+		callback();
+	})
+}
+
 // when new blogposts are created, let tweet
 // npm install mongoose-lifecycle
 // http://plugins.mongoosejs.com?q=events
